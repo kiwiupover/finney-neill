5f6cb6be999b1ee8f3c742108e4a313165fee541
Dave Laird <kiwiupover@gmail.com>
2023-11-12 12:45:27 -0800

    update dependencies and bump nextjs to 14.0.2

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    scrollRestoration: true,
-    appDir: true,
-  },
   images: {
-    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+    ],
   },
   async redirects() {
     return []
